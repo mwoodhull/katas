aee522bfd279a855da4fd3244e6fc0fdce97e79e
mwoodhull <mwoodhull5@gmail.com>
2024-12-19 14:03:55 +0000

    completes task return negative

--- src/return_negative.py
+++ src/return_negative.py
@@ -1,1 +1,10 @@
-def return_negative():
+def return_negative(num):
+    
+    if num == 0:
+        return 0
+    
+    if num > 0:
+        return num - (num * 2)
+    
+    else: 
+        return num
--- tests/test_return_negative.py
+++ tests/test_return_negative.py
@@ -1,1 +1,31 @@
 from src.return_negative import return_negative
+
+def test_0_returns_0():
+    entry = 0
+    result = return_negative(entry)
+
+    assert result == 0
+
+def test_postive_number_returns_negative_number():
+
+    entry = 1
+    result = return_negative(entry)
+
+    assert result == -1
+
+    entry = 43588
+    result = return_negative(entry)
+
+    assert result == -43588
+
+def test_negative_number_returns_negative_numer():
+
+    entry = -1
+    result = return_negative(entry)
+
+    assert result == -1
+
+    entry = -43588
+    result = return_negative(entry)
+
+    assert result == -43588
