a4748284d01aa3140493bd6de602675bf6a11c14
mwoodhull <mwoodhull5@gmail.com>
2024-12-23 14:35:17 +0000

    updates formatting for hello name or world

--- src/hello_name_or_world.py
+++ src/hello_name_or_world.py
@@ -1,8 +1,8 @@
 def hello(name=None):
-    
-    if name == '' or type(name) is not str:
-        return 'Hello, World!'
-    
+
+    if name == "" or type(name) is not str:
+        return "Hello, World!"
+
     cap_name = name.title()
-    
-    return f'Hello, {cap_name}!'
+
+    return f"Hello, {cap_name}!"
--- tests/test_hello_name_or_world.py
+++ tests/test_hello_name_or_world.py
@@ -1,47 +1,53 @@
 from src.hello_name_or_world import hello
 
+
 def test_returns_hello_name_where_name_is_provided():
 
-    entry = 'Matthew'
+    entry = "Matthew"
     output = hello(entry)
 
-    assert output == 'Hello, Matthew!'
+    assert output == "Hello, Matthew!"
+
 
 def test_returns_name_with_name_capitalised_where_not_already_provided():
 
-    entry = 'matthew'
+    entry = "matthew"
     output = hello(entry)
 
-    assert output == 'Hello, Matthew!'
+    assert output == "Hello, Matthew!"
+
 
 def test_returns_name_with_capitalised_first_letter_only_where_all_cap():
 
-    entry = 'mATTHEW'
+    entry = "mATTHEW"
     output = hello(entry)
 
-    assert output == 'Hello, Matthew!'    
+    assert output == "Hello, Matthew!"
+
 
 def test_returns_hello_world_where_name_is_empty_string():
 
-    entry = ''
+    entry = ""
     output = hello(entry)
 
-    assert output == 'Hello, World!'
+    assert output == "Hello, World!"
+
 
 def test_returns_hello_world_where_wrong_data_type():
 
     entry = 66
     output = hello(entry)
 
-    assert output == 'Hello, World!'
+    assert output == "Hello, World!"
 
     entry = True
     output = hello(entry)
 
-    assert output == 'Hello, World!'
+    assert output == "Hello, World!"
+
 
 def test_returns_hello_world_where_no_argument_provided():
 
     output = hello()
 
-    assert output == 'Hello, World!'
+    assert output == "Hello, World!"
