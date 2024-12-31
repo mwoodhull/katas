d084f0094d63f6cee854b1eb1f0537443806aa74
mwoodhull <mwoodhull5@gmail.com>
2024-12-31 09:49:40 +0000

    starts initials testing

---
+++ src/initials.py
@@ -0,0 +1,14 @@
+def initials(name):
+    
+    name_lis = name.split()
+    name_str = ''
+    
+    for name in name_lis:
+        if name is not name_lis[-1]:
+            initial = name[:1]
+            name_str += f'{initial.upper()}.'
+
+        else:
+            name_str += f'{name.title()}'
+
+    return name_str
---
+++ tests/test_initials.py
@@ -0,0 +1,34 @@
+from src.initials import initials
+
+def test_two_word_titled_name_returns_correctly():
+
+    insert = "Code Wars"
+
+    output = initials(insert)
+
+    assert output == "C.Wars"
+
+def test_more_than_two_word_name():
+
+    insert = "Barack Hussein Obama"
+
+    output = initials(insert)
+
+    assert output == "B.H.Obama"
+
+def test_name_provided_all_lower_case():
+
+    insert = "barack hussein obama"
+
+    output = initials(insert)
+
+    assert output == "B.H.Obama"
+
+def test_name_that_is_all_uppercase():
+
+    insert = "BARACK HUSSEIN OBAMA"
+
+    output = initials(insert)
+
+    assert output == "B.H.Obama"
+
