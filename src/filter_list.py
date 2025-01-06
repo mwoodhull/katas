ac004d399d5e74d67a7cfcefe8b31d0f5d669a56
mwoodhull <mwoodhull5@gmail.com>
2025-01-06 17:42:18 +0000

    completes filter list task

---
+++ src/filter_list.py
@@ -0,0 +1,7 @@
+def filter_list(l):
+
+
+    filtered_list = [item for item in l if isinstance(item, int)]
+                
+
+    return filtered_list
---
+++ tests/test_filter_list.py
@@ -0,0 +1,37 @@
+from src.filter_list import filter_list
+
+
+def test_returns_identical_list_when_all_are_integers():
+
+    insert = [1, 2, 3]
+
+    output = filter_list(insert)
+
+    assert output == [1, 2, 3]
+
+
+def test_returns_new_list():
+
+    insert = [1, 2, 3]
+
+    output = filter_list(insert)
+
+    assert output == [1, 2, 3]
+    assert output is not insert
+
+
+def test_returns_empty_list_where_all_strings():
+
+    insert = ["a", "b", "c"]
+
+    output = filter_list(insert)
+
+    assert output == []
+
+def test_returns_only_numbers_where_mix_of_data_types():
+
+    insert = ['a', 1, 6, 3, 1.4, [1], 'b']
+
+    output = filter_list(insert)
+
+    assert output == [1, 6, 3]
