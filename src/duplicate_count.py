a86cd5b01318e7f60f4da1749f16602be1dd7c9d
mwoodhull <mwoodhull5@gmail.com>
2025-01-07 19:08:51 +0000

    completes duplicate count task

--- src/duplicate_count.py
+++ src/duplicate_count.py
@@ -1,5 +1,11 @@
 def duplicate_count(text):
-    
+
     count = 0
+    l_text = text.lower().replace(" ", "")
+
+    for char in l_text:
+        if len(l_text) - len(l_text.replace(char, "")) >= 2:
+            count += 1
+            l_text = l_text.replace(char, "")
 
     return count
