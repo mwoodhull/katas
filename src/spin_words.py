51cd68bd00afd112a4878126ec35eda93f178b69
mwoodhull <mwoodhull5@gmail.com>
2025-01-08 19:54:20 +0000

    completes spin words task

--- src/spin_words.py
+++ src/spin_words.py
@@ -1,3 +1,11 @@
 def spin_words(sentence):
+
+    list_sentence = sentence.split()
+
+    for i in range(len(list_sentence)):
+        if len(list_sentence[i]) >= 5:
+            list_sentence[i] = list_sentence[i][::-1]
+
+    final_sentence = ' '.join(list_sentence)
     
-    return sentence
+    return final_sentence
--- tests/test_spin_words.py
+++ tests/test_spin_words.py
@@ -7,3 +7,27 @@ def test_returns_string_where_all_under_5_letters():
     output = spin_words(insert)
 
     assert output == 'hi this is a test'
+
+def test_returns_flipped_single_word():
+
+    insert = "hello"
+
+    output = spin_words(insert)
+
+    assert output == 'olleh'
+
+def test_returns_flipped_words_in_sentence_of_long_words():
+
+    insert = 'hello fantastic meeting everyone'
+
+    output = spin_words(insert)
+
+    assert output == 'olleh citsatnaf gniteem enoyreve'
+
+def test_returns_mix_of_flipped_words_for_varied_sentence():
+
+    insert = 'hello my name is matthew'
+
+    output = spin_words(insert)
+
+    assert output == 'olleh my name is wehttam'
