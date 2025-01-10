374f2c24901d5cb97fa58bcfdb6145cd0e81fed8
mwoodhull <mwoodhull5@gmail.com>
2025-01-10 11:03:41 +0000

    completes task mumbling

---
+++ src/mumbling.py
@@ -0,0 +1,10 @@
+def accum(st):
+    
+    ls_st = list(st.lower())
+
+    for i in range(0, len(ls_st)):
+
+        ls_st[i] = ls_st[i] * (i+1)
+        ls_st[i] = ls_st[i].title()
+
+    return '-'.join(ls_st)
---
+++ tests/test_mumbling.py
@@ -0,0 +1,41 @@
+from src.mumbling import accum
+
+def test_returns_single_letter_for_single_letter():
+
+    insert = 'A'
+
+    output = accum(insert)
+
+    assert output == 'A'
+
+def test_returns_capitalised_single_letter():
+
+    insert = 'a'
+
+    output = accum(insert)
+
+    assert output == 'A'
+
+def test_returns_correct_response_for_two_letter_string():
+
+    insert = 'ab'
+
+    output = accum(insert)
+
+    assert output == 'A-Bb'
+
+def test_returns_correct_response_for_larger_strings():
+
+    insert = 'abc'
+
+    output = accum(insert)
+
+    assert output == 'A-Bb-Ccc'
+
+def test_returns_correct_response_when_given_mix_of_upper_and_lowercase():
+
+    insert = 'mAtT'
+
+    output = accum(insert)
+
+    assert output == 'M-Aa-Ttt-Tttt'
