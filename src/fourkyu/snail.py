a6426cfd5f2fb28c9752f81d9c119a25d80ce108
mwoodhull <mwoodhull5@gmail.com>
2025-01-12 13:39:52 +0000

    completes first 3 tests of snail task

--- src/fourkyu/snail.py
+++ src/fourkyu/snail.py
@@ -1,2 +1,55 @@
 def snail(snail_map):
-    pass
+
+    if len(snail_map) < 3:
+        for i in snail_map:
+            if i == snail_map[0]:
+                decoded_map = snail_map[0]
+        
+            elif i == snail_map[-1]:
+                for num in i[::-1]:
+                    decoded_map.append(num)
+
+        return decoded_map
+
+    if len(snail_map) == 3:
+
+        for i in snail_map:
+            if i == snail_map[0]:
+                decoded_map = snail_map[0]
+            
+            elif i == snail_map[-1]:
+                for num in i[::-1]:
+                    decoded_map.append(num)
+
+                for lst in snail_map[1][:-1]:
+                    decoded_map.append(lst)
+
+            else:
+                decoded_map.append(i[-1])
+        
+        return decoded_map
+
+    if len(snail_map) > 3:
+
+        for i in snail_map:
+            if i == snail_map[0]:
+                decoded_map = snail_map[0]
+            
+            elif i == snail_map[-1]:
+                for num in i[::-1]:
+                    decoded_map.append(num)
+                    
+
+                for lst in snail_map[::-1]:
+                    if lst[0] not in decoded_map:
+                        decoded_map.append(lst[0])
+                    else:
+                        for num in lst:
+                            decoded_map.append(num)
+                    
+                decoded_map.pop()
+
+            else:
+                decoded_map.append(i[-1])
+        
+        return decoded_map
--- tests/test_4kyu/test_snail.py
+++ tests/test_4kyu/test_snail.py
@@ -1,1 +1,36 @@
 from src.fourkyu.snail import snail
+
+def test_single_nested_list_returns_unnested_list():
+
+    insert = [[1]]
+
+    output = snail(insert)
+
+    assert output == [1]
+
+def test_2_by_2_nested_lists_can_return_correct_response():
+
+    insert = [[1, 2], [3, 4]]
+
+    output = snail(insert)
+
+    assert output == [1, 2, 4, 3]
+
+def test_3_by_3_nested_lists_can_return_correct_response():
+
+    insert = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    output = snail(insert)
+
+    assert output == [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+def test_bigger_than_3_by_3_list_can_return_correct_response():
+
+    insert = [[1, 2, 3, 4], 
+              [5, 6, 7, 8], 
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
+
+    output = snail(insert)
+
+    assert output == [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
