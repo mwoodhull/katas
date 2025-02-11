dad3acc763729663fc32f7c6dce85a616c88cf75
mwoodhull <mwoodhull5@gmail.com>
2025-02-11 10:35:22 +0000

    completes roman numerals task

--- src/fourkyu/roman_numerals.py
+++ src/fourkyu/roman_numerals.py
@@ -1,52 +1,56 @@
 class RomanNumerals:
     @staticmethod
-    def to_roman(val : int) -> str:
-        numeral_units = {1: 'I',
-                    2: 'II',
-                    3: 'III',
-                    4: 'IV',
-                    5: 'V',
-                    6: 'VI',
-                    7: 'VII',
-                    8: 'VIII',
-                    9: 'IX'}
-        
-        numeral_tens = {10: 'X',
-                        20: 'XX',
-                        30: 'XXX',
-                        40: 'XL',
-                        50: 'L',
-                        60: 'LX',
-                        70: 'LXX',
-                        80: 'LXXX',
-                        90: 'XC'}
-        
-        numeral_hunds = {100: 'C',
-                        200: 'CC',
-                        300: 'CCC',
-                        400: 'CD',
-                        500: 'D',
-                        600: 'DC',
-                        700: 'DCC',
-                        800: 'DCCC',
-                        900: 'CM'}
-        
-        numeral_thous = {1000: 'M',
-                        2000: 'MM',
-                        3000: 'MMM'}
+    def to_roman(val: int) -> str:
+        numeral_units = {
+            1: "I",
+            2: "II",
+            3: "III",
+            4: "IV",
+            5: "V",
+            6: "VI",
+            7: "VII",
+            8: "VIII",
+            9: "IX",
+        }
+
+        numeral_tens = {
+            10: "X",
+            20: "XX",
+            30: "XXX",
+            40: "XL",
+            50: "L",
+            60: "LX",
+            70: "LXX",
+            80: "LXXX",
+            90: "XC",
+        }
+
+        numeral_hunds = {
+            100: "C",
+            200: "CC",
+            300: "CCC",
+            400: "CD",
+            500: "D",
+            600: "DC",
+            700: "DCC",
+            800: "DCCC",
+            900: "CM",
+        }
+
+        numeral_thous = {1000: "M", 2000: "MM", 3000: "MMM"}
 
         num_lst = list(map(int, str(val)))
         new_lst = []
         multi = 1
         numerals_lst = []
 
-        for i in range(len(num_lst) -1, -1, -1):
+        for i in range(len(num_lst) - 1, -1, -1):
             if num_lst[i] != 0:
                 new_lst.insert(0, num_lst[i] * multi)
                 multi *= 10
             else:
                 multi *= 10
-            
+
         for num in new_lst:
             if num in numeral_units:
                 numerals_lst.append(numeral_units[num])
@@ -63,38 +67,40 @@ class RomanNumerals:
         return "".join(numerals_lst)
 
     @staticmethod
-    def from_roman(roman_num : str) -> int:
-        numeral_dict = {'I': 1,
-                        'II': 2,
-                        'III': 3,
-                        'IV': 4,
-                        'V': 5,
-                        'VI': 6,
-                        'VII': 7,
-                        'VIII': 8,
-                        'IX': 9,
-                        'X': 10,
-                        'XX': 20,
-                        'XXX': 30,
-                        'XL': 40,
-                        'L': 50,
-                        'LX': 60,
-                        'LXX': 70,
-                        'LXXX': 80,
-                        'XC': 90,
-                        'C': 100,
-                        'CC': 200,
-                        'CCC': 300,
-                        'CD': 400,
-                        'D': 500,
-                        'DC': 600,
-                        'DCC': 700,
-                        'DCCC': 800,
-                        'CM': 900,
-                        'M': 1000,
-                        'MM': 2000,
-                        'MMM': 3000}
-        
+    def from_roman(roman_num: str) -> int:
+        numeral_dict = {
+            "I": 1,
+            "II": 2,
+            "III": 3,
+            "IV": 4,
+            "V": 5,
+            "VI": 6,
+            "VII": 7,
+            "VIII": 8,
+            "IX": 9,
+            "X": 10,
+            "XX": 20,
+            "XXX": 30,
+            "XL": 40,
+            "L": 50,
+            "LX": 60,
+            "LXX": 70,
+            "LXXX": 80,
+            "XC": 90,
+            "C": 100,
+            "CC": 200,
+            "CCC": 300,
+            "CD": 400,
+            "D": 500,
+            "DC": 600,
+            "DCC": 700,
+            "DCCC": 800,
+            "CM": 900,
+            "M": 1000,
+            "MM": 2000,
+            "MMM": 3000,
+        }
+
         numeral_list = list(roman_num)
         integer_list = []
         total = 0
@@ -102,13 +108,13 @@ class RomanNumerals:
 
         for rom_num in numeral_list:
             integer_list.append(numeral_dict[rom_num])
-            
-        for i in range(len(integer_list) -1):
-            if integer_list[i] < integer_list[i+1]:
+
+        for i in range(len(integer_list) - 1):
+            if integer_list[i] < integer_list[i + 1]:
                 total -= integer_list[i]
-                
+
             else:
                 total += integer_list[i]
-                
+
         total += integer_list[-1]
         return total
--- tests/test_4kyu/test_roman_numerals.py
+++ tests/test_4kyu/test_roman_numerals.py
@@ -1,82 +1,101 @@
 from src.fourkyu.roman_numerals import RomanNumerals
 
+
 def test_one_returns_I():
 
-    assert RomanNumerals.to_roman(1) == 'I'
+    assert RomanNumerals.to_roman(1) == "I"
 
-    assert RomanNumerals.from_roman('I') == 1
+    assert RomanNumerals.from_roman("I") == 1
 
-    assert RomanNumerals.to_roman(7) == 'VII'
+    assert RomanNumerals.to_roman(7) == "VII"
 
 
 def test_int_to_numerals_tens_returns_correct_response():
 
-    assert RomanNumerals.to_roman(30) == 'XXX'
+    assert RomanNumerals.to_roman(30) == "XXX"
 
-    assert RomanNumerals.to_roman(21) == 'XXI'
+    assert RomanNumerals.to_roman(21) == "XXI"
 
-    assert RomanNumerals.to_roman(17) == 'XVII'
+    assert RomanNumerals.to_roman(17) == "XVII"
 
-    assert RomanNumerals.to_roman(49) == 'XLIX'
+    assert RomanNumerals.to_roman(49) == "XLIX"
 
-    assert RomanNumerals.to_roman(85) == 'LXXXV'
+    assert RomanNumerals.to_roman(85) == "LXXXV"
+
 
 def test_int_to_numerals_hundreds_returns_correct_response():
 
-    assert RomanNumerals.to_roman(300) == 'CCC'
+    assert RomanNumerals.to_roman(300) == "CCC"
 
-    assert RomanNumerals.to_roman(211) == 'CCXI'
+    assert RomanNumerals.to_roman(211) == "CCXI"
 
-    assert RomanNumerals.to_roman(917) == 'CMXVII'
+    assert RomanNumerals.to_roman(917) == "CMXVII"
 
-    assert RomanNumerals.to_roman(449) == 'CDXLIX'
+    assert RomanNumerals.to_roman(449) == "CDXLIX"
 
-    assert RomanNumerals.to_roman(585) == 'DLXXXV'
+    assert RomanNumerals.to_roman(585) == "DLXXXV"
+
 
 def test_int_to_numerals_thousands_returns_correct_response():
 
-    assert RomanNumerals.to_roman(1300) == 'MCCC'
+    assert RomanNumerals.to_roman(1300) == "MCCC"
 
-    assert RomanNumerals.to_roman(2211) == 'MMCCXI'
+    assert RomanNumerals.to_roman(2211) == "MMCCXI"
 
-    assert RomanNumerals.to_roman(3917) == 'MMMCMXVII'
+    assert RomanNumerals.to_roman(3917) == "MMMCMXVII"
+
 
 def test_1001_or_101_returns_correct_response():
 
-    assert RomanNumerals.to_roman(1001) == 'MI'
+    assert RomanNumerals.to_roman(1001) == "MI"
 
-    assert RomanNumerals.to_roman(101) == 'CI'
+    assert RomanNumerals.to_roman(101) == "CI"
 
-    assert RomanNumerals.to_roman(1031) == 'MXXXI'
+    assert RomanNumerals.to_roman(1031) == "MXXXI"
 
-    assert RomanNumerals.to_roman(1801) == 'MDCCCI'
-
+    assert RomanNumerals.to_roman(1801) == "MDCCCI"
 
 
 def test_numerals_to_int_units_returns_correct_response():
 
-    assert RomanNumerals.from_roman('III') == 3
+    assert RomanNumerals.from_roman("III") == 3
 
-    assert RomanNumerals.from_roman('VII') == 7
+    assert RomanNumerals.from_roman("VII") == 7
 
-    assert RomanNumerals.from_roman('IX') == 9
+    assert RomanNumerals.from_roman("IX") == 9
+
 
 def test_numerals_to_int_tens_returns_correct_response():
 
-    assert RomanNumerals.from_roman('XV') == 15
+    assert RomanNumerals.from_roman("XV") == 15
 
-    assert RomanNumerals.from_roman('XXXI') == 31
+    assert RomanNumerals.from_roman("XXXI") == 31
 
-    assert RomanNumerals.from_roman('XCIX') == 99
+    assert RomanNumerals.from_roman("XCIX") == 99
 
-#     3856
 
-#     6
-#     50
-#     800
-#     3000
+def test_numerals_to_int_hundreds_returns_correct_response():
 
-#     VI
-#     LVI
-#     DCCCLVI
-#     MMMDCCCLVI
+    assert RomanNumerals.from_roman("CXIII") == 113
+
+    assert RomanNumerals.from_roman("CCCXLIX") == 349
+
+    assert RomanNumerals.from_roman("CDXCIV") == 494
+
+    assert RomanNumerals.from_roman("CMXCIX") == 999
+
+def test_numerals_to_int_thousands_returns_correct_response():
+
+    assert RomanNumerals.from_roman("MCXI") == 1111
+
+    assert RomanNumerals.from_roman("MCDLIX") == 1459
+
+    assert RomanNumerals.from_roman("MMMD") == 3500
+
+    assert RomanNumerals.from_roman("MMMCMXLIX") == 3949
+
+def test_numerals_to_int_101_and_1001_returns_correct_response():
+
+    assert RomanNumerals.from_roman("MI") == 1001
+
+    assert RomanNumerals.from_roman("CI") == 101
