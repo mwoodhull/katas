62ca8511f6287e4fafd1584626c4ad6ef13fa1d8
mwoodhull <mwoodhull5@gmail.com>
2025-03-04 22:05:39 +0000

    completes further tests

--- src/interview_prep/reverse_string.py
+++ src/interview_prep/reverse_string.py
@@ -3,3 +3,19 @@ def reverse(words):
     backwards = words[::-1]
 
     return backwards
+
+def find_largest(nums):
+
+    return max(nums)
+
+import math
+
+def is_prime(integer):
+    if integer <= 1:
+        return False
+    
+    for i in range(2, int(math.sqrt(integer)) + 1):
+        if integer % i == 0:
+            return False
+        
+    return True
--- tests/interview_prep_tests/test_reverse_string.py
+++ tests/interview_prep_tests/test_reverse_string.py
@@ -1,4 +1,6 @@
 from src.interview_prep.reverse_string import reverse
+from src.interview_prep.reverse_string import find_largest
+from src.interview_prep.reverse_string import is_prime
 
 
 def test_returns_reversed_string():
@@ -8,3 +10,23 @@ def test_returns_reversed_string():
     output = reverse(insert)
 
     assert output == 'olleh'
+
+def test_find_largest_works():
+
+    insert = [4, 6, 9, 11, 2]
+
+    output = find_largest(insert)
+
+    assert output == 11
+
+def test_finding_prime():
+
+    insert = 6
+    output = is_prime(insert)
+
+    assert output is False
+
+    insert = 7
+    output = is_prime(insert)
+
+    assert output is True
